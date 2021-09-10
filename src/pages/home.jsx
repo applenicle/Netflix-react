cb1a707f5e7a9d1000bb4eb46f90dcb13e5c13db
applenicle <52568225+applenicle@users.noreply.github.com>
2021-09-10 18:22:43 +0500

    Верстка 2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Feature, OptForm} from '../components'
+
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 import {JumbotronContainer} from '../containers/jumbotron';
 
 export default function Home() {
   return ( 
   <>
-        <JumbotronContainer/>
-        <FaqsContainer/>
-        <FooterContainer/>
+        <HeaderContainer>
+          <Feature>
+          <Feature.Title>Unlimited films, Tv programmers adn more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at ant time</Feature.SubTitle>
+          </Feature>
+            <OptForm>
+              <OptForm.Input placeholder="Email adress"/>
+              <OptForm.Button>Try it now</OptForm.Button>
+              <OptForm.Break/>
+              <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+          </OptForm>
+        </HeaderContainer>
+          <JumbotronContainer/>
+          <FaqsContainer/>
+          <FooterContainer/>
+        
    </>
    );
 } 
