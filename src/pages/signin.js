2ae20effe704b5a0a7f5bfdec5469b7bf537c5cf
applenicle <52568225+applenicle@users.noreply.github.com>
2021-09-28 17:02:26 +0500

    Sign in

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer} from '../containers/header'
+import { Form } from '../components'
+import * as ROUTES from '../constants/routes'
 
 export default  function Signin() {
+  const history = useHistory ();
+  const { firebase } = React.useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [error, setError] = React.useState('')
+
+  const isInvalid = password === '' || emailAddress === ''; 
+  const handleSignin = (event) => {
+    event.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message)
+      });
+  };
   return (
     <>
-    <HeaderContainer></HeaderContainer>
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign In</Form.Title>{error && <Form.Error>{error}</Form.Error>}
+        <Form.Base onSubmit={handleSignin} method='POST'>
+          <Form.Input placeholder="Email addres"
+          value={emailAddress}
+          onChange={({ target }) => setEmailAddress(target.value)}/>
+          <Form.Input placeholder="Password"
+          autoComplete="off"
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}/>
+          <Form.Submit disabled={isInvalid} type="submit">
+            Sign in
+          </Form.Submit>
+          
+        </Form.Base>
+        <Form.Text>
+          New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+        </Form.Text>
+        <Form.TextSmall>This page is protected by Google reCAPTCHA to ensure
+          you're not a bot. Learn more.
+        </Form.TextSmall>
+      </Form>
+    </HeaderContainer>
     <FooterContainer/>
     </>
   );
