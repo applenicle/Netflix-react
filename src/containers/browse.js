037a1b972a6b46a35b64278c0965a2841bec9299
applenicle <52568225+applenicle@users.noreply.github.com>
2021-10-02 23:56:12 +0500

    Header profile

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext} from '../context/firebase'
-import { Loading } from '../components'
+import { Header, Loading } from '../components'
+import * as ROUTES  from '../constants/routes'
+import logo from '../logo.svg'
 
 export function BrowseContainer({ slides }) {
+  const [searchTerm, setSearchTerm] = React.useState('');
   const [profile, setProfile] = React.useState({});
   const [loading, setLoading] = React.useState(true);
   const { firebase } = React.useContext(FirebaseContext);
@@ -16,7 +19,48 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (<Loading src={user.photoURL} />) : null)
-   :
+    <>
+    {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+    <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>
+              Series
+            </Header.TextLink>
+            <Header.TextLink>
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search/>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+            futile attempt to feel like he's part of the world around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+    </>
+   ):
     (<SelectProfileContainer user={user} setProfile={setProfile}/>);
 }
