3adadd4d683393c3307e3cdf746e52abd9f19a59
applenicle <52568225+applenicle@users.noreply.github.com>
2021-10-03 16:42:53 +0500

    Card

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import { SelectProfileContainer } from './profiles';
-import { FirebaseContext} from '../context/firebase'
-import { Header, Loading } from '../components'
+import React, {useEffect} from 'react'
+import Fuse from 'fuse.js';
+import { Card, Header, Loading } from '../components'
 import * as ROUTES  from '../constants/routes'
 import logo from '../logo.svg'
+import { SelectProfileContainer } from './profiles';
+import { FirebaseContext} from '../context/firebase'
+import { FooterContainer } from './footer';
+
 
 export function BrowseContainer({ slides }) {
+  const [category, setCategory] = React.useState('series');
   const [searchTerm, setSearchTerm] = React.useState('');
   const [profile, setProfile] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+  const [slideRows, setSlideRows] = React.useState([]);
+
   const { firebase } = React.useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {}; 
 
-  React.useEffect(() => {
+  useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, [profile.displayName]);
+
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [slides, category]);
+
 
   return profile.displayName ? (
     <>
@@ -26,10 +37,10 @@ export function BrowseContainer({ slides }) {
         <Header.Frame>
           <Header.Group>
             <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-            <Header.TextLink>
+            <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
               Series
             </Header.TextLink>
-            <Header.TextLink>
+            <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
               Films
             </Header.TextLink>
           </Header.Group>
@@ -60,6 +71,32 @@ export function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            {/* <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
+            </Card.Feature> */}
+          </Card>
+        ))}
+      </Card.Group>
+      <FooterContainer />
     </>
    ):
     (<SelectProfileContainer user={user} setProfile={setProfile}/>);
